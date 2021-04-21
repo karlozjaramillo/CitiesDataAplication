91502b1d79da012f93a2f932ae512b3a95b6bbf8
Karloz <karloz.jaramillo@hotmail.com>
2021-04-21 11:16:05 -0500

    se agrega la clase Cities.java

---
+++ app/src/main/java/com/cj/citiesdataaplication/Cities.java
@@ -0,0 +1,69 @@
+package com.cj.citiesdataaplication;
+
+public class Cities {
+    int id;
+    String nombre;
+    int poblacion;
+    double latitud;
+    double longitud;
+
+    public Cities() {
+    }
+
+    public Cities(int id, String nombre, int poblacion, double latitud, double longitud) {
+        this.id = id;
+        this.nombre = nombre;
+        this.poblacion = poblacion;
+        this.latitud = latitud;
+        this.longitud = longitud;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPoblacion() {
+        return poblacion;
+    }
+
+    public void setPoblacion(int poblacion) {
+        this.poblacion = poblacion;
+    }
+
+    public double getLatitud() {
+        return latitud;
+    }
+
+    public void setLatitud(double latitud) {
+        this.latitud = latitud;
+    }
+
+    public double getLongitud() {
+        return longitud;
+    }
+
+    public void setLongitud(double longitud) {
+        this.longitud = longitud;
+    }
+
+    @Override
+    public String toString() {
+        return "Id: " + id +
+                "Nombre: " + nombre +
+                "Poblacion: " + poblacion +
+                "Latitud: " + latitud +
+                "Longitud: " + longitud;
+    }
+}
